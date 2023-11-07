0c158f5ec1ce1f030fb5b2717cdb3702b37a5e37
AlexSohn <sohnfambly@gmail.com>
2023-11-07 11:51:36 -0500

    Updated Milestone1 Code

--- Milestone 1 Code.py
+++ Milestone 1 Code.py
@@ -1,1 +1,11 @@
 from dataclasses import dataclass
+from designer import *
+from random import randint
+
+@dataclass
+class World:
+    player_speed:int
+    player_icon:DesignerObject
+
+
+#def create_world() -> World:
