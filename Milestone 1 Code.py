7c3b542e119997e5801f411c2bd5bbe66d68321d
nareensulatana <nareensulatana@gmail.com>
2023-11-08 01:34:03 -0500

    This is Nareen's branch

--- Milestone 1 Code.py
+++ Milestone 1 Code.py
@@ -1,4 +1,55 @@
 from dataclasses import dataclass
+from designer import *
+from random import randint
+
 
 @dataclass
 class World:
+    obstacle_speed: int
+    player_icon: DesignerObject
+    in_space: bool
+
+
+def create_playericon() -> DesignerObject:
+    """Creates the bird icon for the player"""
+    bird = emoji("bird")
+    bird.y = 550
+    bird.x = 100
+    bird.flip_x = True
+    return bird
+
+
+def movement(world: World):
+    """Makes sure that the world is constantly moving"""
+    if world.in_space:
+        world.player_icon.y -= 100
+    else:
+        world.player_icon.y += 2
+
+
+
+def create_world()->World:
+    """Creates the world for game"""
+    player_icon = create_playericon()
+    return World(5, player_icon,False)
+
+
+def jump(world: World):
+    world.in_space = True
+    world.player_icon.y -= 50
+
+def space_released(world: World):
+    """ Allows the bird to fall when "space" key is released (user stops pressing "space" key"""
+    world.in_space = False
+
+def game_loop(world: World):
+    """Allows the game to have continuous movement of the bird"""
+    movement(world)
+
+
+# when('updating', constant_movement)
+when("typing", jump)
+when('starting', create_world)
+when("typing", space_released)
+when("updating", game_loop)
+start()
