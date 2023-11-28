baeda02fe054becdfcf549af78dd824a7f11a36c
ASohn <129706813+Rhytid@users.noreply.github.com>
2023-11-28 16:55:16 -0500

    Update Milestone 1 Code.py

--- Milestone 1 Code.py
+++ Milestone 1 Code.py
@@ -12,11 +12,12 @@ class World:
     timer: float
     point: int
     counter: DesignerObject
+    balloons: list[DesignerObject]
 
 def create_playericon() -> DesignerObject:
     """Creates the bird icon for the player"""
     bird = emoji("bird")
-    bird.y = 550
+    bird.y = 450
     bird.x = 100
     bird.flip_x = True
     return bird
@@ -31,7 +32,7 @@ def player_movement(world: World):
 def create_world() -> World:
     """Creates the world for the game"""
     player_icon = create_playericon()
-    return World(5, player_icon, False, [], time.time(), 0, text("black"," 0",20,400,65))
+    return World(5, player_icon, False, [], time.time(), 25, text("black"," 0",20,400,65), [])
 
 def jump(world: World):
     """Allows the Bird to fly up into the air"""
@@ -60,15 +61,15 @@ def collision(world: World) -> bool:
             collides = True
     return collides
 
-def obstacle_spawn(world:World):
+def generate_obstacle(world:World):
     """ Creation of the icons that will be the obstacle"""
-    if 10>world.point >= 5:
+    if 10> world.point >= 5:
         obstacle = emoji('rock')
     elif 15>world.point >= 10:
         obstacle = emoji("baby")
     elif 20>world.point >= 15:
         obstacle = emoji("dog")
-    elif world.obstacle_speed >= 20:
+    elif world.point >= 20:
         obstacle = emoji("cloud")
     else:
         obstacle = emoji("bug")
@@ -79,10 +80,29 @@ def obstacle_spawn(world:World):
     obstacle.y = randint(0, get_height())
     return obstacle
 
-def obstacles(world: World):
+def celebration(world:World):
+    """Spawns celebratory balloons if the player makes it to the final level"""
+    if world.point >= 20:
+        if len(world.balloons) < 11:
+            world.balloons.append(balloon_creation(world))
+
+def balloon_creation(world:World):
+    """Creates the balloon emoji"""
+    balloon = emoji("balloon")
+    balloon.scale_x = 2
+    balloon.scale_y = 2
+    balloon.x = randint(0,get_width())
+    balloon.y = get_height()
+    return balloon
+
+def balloon_movement(world: World):
+    """Moves the balloons"""
+    for balloon in world.balloons:
+        balloon.y -= randint(5,15)
+def obstacle_creation(world: World):
     """Creating the obstacle that the player icon will have to avoid hitting"""
     if len(world.obstacles) < 1:
-        world.obstacles.append(obstacle_spawn(world))
+        world.obstacles.append(generate_obstacle(world))
 
 def obstacle_movement(world: World):
     """Moves the obstacles"""
@@ -101,12 +121,21 @@ def obstacle_deletion(world: World):
             world.obstacle_speed += 1.5
     world.obstacles = newlist
     
+def balloon_deletion(world:World):
+    """Removes excess balloons"""
+    newlist =[]
+    for balloon in world.balloons:
+        if balloon.y > 10:
+            newlist.append(balloon)
+        else:
+            destroy(balloon)
+    world.balloons = newlist
 
 
 def display_points(world):
     """Show the point value"""
     world.counter.text = "Points: " + str(world.point)
-    print(world.player_icon.y)
+    #print(world.player_icon.y)
     
 def game_over(world):
     """ Show the game over message """
@@ -116,10 +145,13 @@ when("typing", jump)
 when('starting', create_world)
 when("typing", space_released)
 when("updating", game_loop)
-when("updating", obstacle_deletion)
+when("updating", celebration)
 when("updating", player_movement)
+when("updating", obstacle_creation)
 when("updating", obstacle_movement)
-when("updating", obstacles)
+when("updating", balloon_movement)
 when("updating", display_points)
+when("updating", obstacle_deletion)
+when("updating", balloon_deletion)
 when(collision, game_over, pause)
 start()
